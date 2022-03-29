8d68a23e19cf49186c6d1089874d8863bba3a849
Ben <banthony446@gmail.com>
2022-03-29 14:36:35 -0400

    updated styling

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Phase-1 Project
+
+1. turn the api into a local db.json file within this project
+2. when the timer runs out, trivia game runs the same function that is run when the user triggers the submit event via the submit button
+3. when the timer runs out/submit event is triggered, create a delete request that deletes the translated nation title from the local db.json file (ex: china in swedish. once it is used, deletes china in swedish from local db.json file)
+4. the start over/ go back to how to play button submits a fetch request of the data from the api to the db.json file and REPLACES the existing information within (which will reset the information that can be pulled, ex: china in swedish will, by proxy, be added back to the pool of grabbable translated nation names to use in the trivia game).
--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+// Variables
 const countryList = document.getElementById('country-container')
+const triviaContainer = document.getElementById('trivia-container')
+const studyBttn = document.getElementById('study-button')
+const triviaBttn = document.getElementById('quiz-button')
+const howToPlay = document.getElementById('how-to-play')
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(resp => resp.json())
-    .then(countries => countries.forEach(renderCountries))
-})
+const fetchCountries = () => {
+    return fetch('https://restcountries.com/v3.1/all')
+        .then(resp => resp.json())
+}
 
+// create Card for each Country
 const renderCountries = (country) => {
     const countryCard = document.createElement('div')
     countryCard.className = 'card'
 
+    const div = document.createElement('div')
+    div.className = 'flag-container'
     const countryFlag = document.createElement('img')
     countryFlag.src = country.flags.svg
+    countryFlag.class = 'flags'
+    div.append(countryFlag)
 
     const countryName = document.createElement('h3')
     countryName.textContent = country.name.common
@@ -22,6 +31,59 @@ const renderCountries = (country) => {
     const continent = document.createElement('p')
     continent.textContent = `Continent: ${country.continents}`
 
-    countryCard.append(countryFlag, countryName, population, continent)
+    countryCard.append(div, countryName, population, continent)
     countryList.appendChild(countryCard)
 }
+
+// trivia page
+const renderTrivia = () => {
+    const triviaGame = document.createElement('div')
+    triviaGame.id = 'trivia-game'
+
+    const hintBttn = document.createElement('h3')
+    hintBttn.id = 'hint-button'
+    hintBttn.textContent = ' hint? '
+
+    const startOver = document.createElement('button')
+    startOver.id = 'start-over'
+    startOver.textContent = 'START OVER'
+
+    const timer = document.createElement('h1')
+    timer.textContent = 30
+    timer.id = 'timer'
+
+    const form = document.createElement('form')
+    form.id = 'form'
+    const h3 = document.createElement('h3')
+    h3.textContent = 'Answer:'
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.name = 'answer'
+    input.placeholder = 'Type answer here'
+    form.append(h3, input)
+
+    const scoreboard = document.createElement('div')
+    scoreboard.id = 'scoreboard'
+    const h2 = document.createElement('h2')
+    h2.textContent = 'Scoreboard: '
+    scoreboard.append(h2)
+
+    triviaGame.append(hintBttn, startOver, timer, form, scoreboard)
+    triviaContainer.append(triviaGame)
+}
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', fetchCountries)
+studyBttn.addEventListener('click', () => {
+    howToPlay.style.display = 'none'
+    countryList.replaceChildren()
+    triviaContainer.replaceChildren()
+    fetchCountries()
+    .then(countries => countries.forEach(renderCountries))
+})
+triviaBttn.addEventListener('click', () => {
+    howToPlay.style.display = 'none'
+    countryList.replaceChildren()
+    triviaContainer.replaceChildren()
+    renderTrivia()
+})
